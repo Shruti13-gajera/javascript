34701c4a9aee7c5b0c23e3bc01dfae041f9fb72e
shruti31 <shrutigajera6@gmail.com>
2025-06-24 13:08:26 +0000

    varaiable and constant

---
+++ sjs/var.js
@@ -0,0 +1,7 @@
+const accountId = 1468
+let accountEmail = "shrutigajera6@gmail.com"
+var accountPassword = "shruti123"
+accountCity = "surat"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
