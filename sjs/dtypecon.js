281d0eb2f570057dd303b0e08a598b281c85bb7f
shruti31 <shrutigajera6@gmail.com>
2025-06-25 09:02:13 +0000

    coversion of data type

---
+++ sjs/dtypecon.js
@@ -0,0 +1,10 @@
+let score = "shruti"
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let shruti = 1
+let booleanShruti = Boolean(shruti)
+console.log(booleanShruti)
