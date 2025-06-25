c44b614a47571cf13a6f167dc8f0dd28d30e3ef1
shruti31 <shrutigajera6@gmail.com>
2025-06-25 09:20:22 +0000

    operations

---
+++ sjs/ope.js
@@ -0,0 +1,17 @@
+console.log(2 + 2)
+console.log(2 - 2)
+console.log(2 * 2)
+console.log(2 / 2)
+console.log(2 % 2) 
+
+let str1 = "shruti"
+let str2 = " gajera"
+let str3 = str1 + str2
+console.log(str3)
+
+
+console.log(+true)
+
+let num = 100
+let num1 = ++num
+console.log(num1)
