062216ba8309dd1f263b3717cc6c5763ac2df4cc
shruti31 <shrutigajera6@gmail.com>
2025-06-28 12:39:52 +0000

    date and time

---
+++ sjs/date.js
@@ -0,0 +1,16 @@
+let date = new Date()
+console.log(date);
+
+console.log(date.getDate());
+console.log(date.toLocaleString());
+console.log(date.toString());
+
+
+let myDate = new Date(2025,4,30)
+console.log(myDate);
+console.log(myDate.getDate());
+console.log(myDate.toDateString());
+let myTimeStemp = Date.now()
+console.log(myTimeStemp);
+console.log(Math.floor(Date.now()/1000));
+
