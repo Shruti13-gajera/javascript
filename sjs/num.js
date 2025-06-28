2c3d453633dd24a20ba17fa249dd9dc2ffe0c25d
shruti31 <shrutigajera6@gmail.com>
2025-06-28 11:28:24 +0000

    number

---
+++ sjs/num.js
@@ -0,0 +1,34 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(500)
+console.log(balance);
+
+console.log(balance.toString().length);
+
+const doller = 1000000
+console.log(doller.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++++++++++++  MATH  +++++++++++++++++++++++++++++++++++++++++++++//
+
+console.log(Math);
+
+console.log(Math.min(82,92));
+console.log(Math.max(82,92));
+console.log(Math.abs(-78));
+
+console.log(Math.random());
+
+console.log(Math.random() * 10);
+console.log((Math.random() * 10 ) + 1);
+
+const max = 20
+const min = 10
+// importnt equation//
+console.log(Math.floor(Math.random() * (max - min + 1)));
+
+
+
+
+
