7b41fe6d02947b30059d3eef8c7ac87c19a8b840
shruti31 <shrutigajera6@gmail.com>
2025-07-15 11:25:08 +0530

    Update obj.js

--- sjs/obj.js
+++ sjs/obj.js
@@ -10,4 +10,13 @@ const intro = {
 console.log(intro)
 console.log(intro[mysym])
 console.log(intro.email)
+const greeting = function(){
+    console.log("hello")
+}
 
+const greeting1 = function(){
+    console.log('hello,${this.name}')
+}
+
+console.log(jsuser.greeting());
+console.log(jsuser.gretting1());
